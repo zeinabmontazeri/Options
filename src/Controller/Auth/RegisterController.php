30877c49923e086a257fbe6e6066fb3c10d05501
mehdad <m.mohamadi313@gmail.com>
2022-08-22 08:41:10 +0430

    Add new controller and service  for register user

---
+++ src/Controller/Auth/RegisterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use App\Exception\ValidationException;
+use App\Repository\UserRepository;
+use App\Request\UserRegisterRequest;
+use App\Service\UserRegisterService;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+#[Route('/auth/', name: 'auth.')]
+class RegisterController extends AbstractController
+{
+    #[Route('register', name: 'register',methods:['post'])]
+    public function index(ValidatorInterface $validator,
+    UserRegisterService $userRegisterService,
+    UserRepository $userRepository,
+    UserPasswordHasherInterface $hasher
+    ): Response
+    {
+        try {
+            $validatedRequest = new  UserRegisterRequest($validator);
+            $res = $userRegisterService->register($validatedRequest, $userRepository,$hasher);
+            return $this->json([
+                'data' => $res,
+                'message' => 'User created successfully',
+                'status' => 'success',
+            ], 200);
+        } catch (ValidationException $e) {
+            return $this->json([
+                'error'=>$e->getMessage(),
+                'data'=>$e->getMessages(),
+            ],400);
+        } catch (Exception $e) {
+            return $this->json([
+                'error'=>$e->getMessage(),
+                'data'=>null,
+            ],400);
+        }
+        return $this->json(["a"=>50]);
+    }
+}
--- src/Service/UserRegisterService.php
+++ src/Service/UserRegisterService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Request\UserRegisterRequest;
+use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
@@ -12,21 +13,26 @@ class UserRegisterService
 {
 
 
-    public function add(
+    public function register(
         UserRegisterRequest         $request,
         UserRepository              $userRepository,
-        UserPasswordHasherInterface $hasher): JsonResponse
+        UserPasswordHasherInterface $hasher): User
     {
+        //Check if user already exists
+        $existing_user = $userRepository->findOneBy(['phoneNumber'=>$request->phoneNumber]);
+        if($existing_user)
+            throw new Exception('User Already Exists');
+            
         $user = new User();
         $user->setPhoneNumber($request->phoneNumber);
-        $user->setPassword($hasher->hashPassword($user, $request->password));
-        $user->setEmail($request->email);
         $user->setFirstName($request->firstName);
         $user->setLastName($request->lastName);
         $user->setBirthDate(new \DateTime($request->birthDate));
-        $user->setCreatedAt((new \DateTimeImmutable)->setTimestamp(time()));
+        $user->setGender($request->gender);
+        $user->setRoles([$request->role]);
+        $user->setPassword($hasher->hashPassword($user, $request->password));
         $userRepository->add($user, true);
-        return new JsonResponse(['message' => 'success'], 201);
+        return $user;
 
     }
 
