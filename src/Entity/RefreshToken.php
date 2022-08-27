f42b855d7cd4ac28ab1c80ebc463acedf43237e0
mehdad <m.mohamadi313@gmail.com>
2022-08-27 13:45:00 +0430

    Fix Refresh token entity

--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -4,10 +4,8 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 
-/**
- * @ORM\Entity
- * @ORM\Table("refresh_tokens")
- */
+#[ORM\Entity()]
+#[ORM\Table("refresh_tokens")]
 class RefreshToken extends BaseRefreshToken
 {
 }
