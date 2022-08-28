b1637be10a404b3554d9ab054e5f9d6a38a227bd
mehdad <m.mohamadi313@gmail.com>
2022-08-28 20:44:29 +0430

    Load refresh token from custom repository

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
