d24490de0729534a343d539d757c6f126301fbd1
mah_zz10@yahoo.com <mah_zz10@yahoo.com>
2022-08-30 12:22:58 +0430

    fix(back):remove user id

--- src/Controller/Shop/OrderController.php
+++ src/Controller/Shop/OrderController.php
@@ -1,21 +1,53 @@
 <?php
 
 namespace App\Controller\Shop;
-
+use App\Auth\AcceptableRoles;
+use App\Auth\AuthenticatedUser;
+use App\Entity\Order;
 use App\Entity\User;
 use App\Repository\OrderRepository;
 use App\Service\Shop\OrderService;
+use App\Service\Shop\RemoveOrderService;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-#[Route('/api/shop')]
+
+#[Route('api/v1/shop')]
 class OrderController extends AbstractController
 {
-    #[Route('/experiencerOrder/{id}', name: 'app_shop_experiencer_order' , methods: 'GET')]
-    public function getExperiencerOrder(OrderService $orderService ,User $user): Response
+    /**
+     * @throws JWTDecodeFailureException
+     */
+    #[Route('/orders/{id}/remove', name: 'app_remove_order', requirements: ['id' => '\d+'], methods: ["DELETE"])]
+    #[AcceptableRoles(User::ROLE_ADMIN, User::ROLE_EXPERIENCER)]
+    public function index(
+        Order              $order,
+        RemoveOrderService $removeOrderService,
+        OrderRepository    $orderRepository,
+        AuthenticatedUser  $security): JsonResponse
     {
-        $res = $orderService->getUserOrders($user->getId());
+        if ($order->getStatus() == 'draft' and $order->getUser() === $security->getUser()) {
+            $removeOrderService->removeOrder($order, $orderRepository);
+            return $this->json([
+                'message' => 'Order Removed Successfully.',
+                'data' => [],
+                'status' => 'success'],
+                Response::HTTP_OK);
+        } else {
+            throw new AccessDeniedHttpException(
+                'You are not allowed to remove this order.');
+        }
+    }
+
+    #[Route('/users/orders', name: 'app_shop_users_order', methods: 'GET')]
+    #[AcceptableRoles(User::ROLE_EXPERIENCER)]
+    public function getExperiencerOrder(OrderService $orderService,AuthenticatedUser $security): Response
+    {
+        $res = $orderService->getUserOrders($security->getUser()->getId());
         return $this->json([
             'data' => $res,
             'status' => true,
--- src/Service/Shop/OrderService.php
+++ src/Service/Shop/OrderService.php
@@ -8,22 +8,11 @@ use App\Repository\UserRepository;
 
 class OrderService
 {
-    private ?User $user = null;
-    public function __construct(private readonly UserRepository $userRepository,
-                                private readonly OrderRepository $orderRepository)
+    public function __construct(private readonly OrderRepository $orderRepository)
     {
     }
     public function getUserOrders($userId)
     {
-        $this->checkUserExistence($userId);
        return $this->orderRepository->getExperiencerOrder($userId);
     }
-    private function checkUserExistence($userId):void
-    {
-        $this->user = $this->userRepository->find($userId);
-        if ($this->user == null) {
-            throw new \Exception('The userId not exists',400);
-        }
-    }
-
 }
