44437c3e35286087c8da774755f0b4bcc33bfc52
S. Sadegh Marashi <s.s.marashi@gmail.com>
2022-09-01 12:54:48 +0430

    feat#10: Update TransactionCmdEnum to support all commands

--- src/Entity/TransactionCmdEnum.php
+++ src/Entity/TransactionCmdEnum.php
@@ -6,6 +6,9 @@ enum TransactionCmdEnum: string
 {
     case Payment = 'CMD_PAYMENT';
     case Verify = 'CMD_VERIFY';
+    case Settle = 'CMD_SETTLE';
     case Inquery = 'CMD_INQUERY';
     case Reversal = 'CMD_REVERSAL';
+    case Refund = 'CMD_REFUND';
+    case PayResponse = 'CMD_PAY_RESPONSE';
 }
