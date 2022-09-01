308124069b9387e32dc9c6239e18d58dab4a6d73
S. Sadegh Marashi <s.s.marashi@gmail.com>
2022-09-01 13:54:33 +0430

    feat#10: Added PaymentCmd

---
+++ src/Payment/Note/PaymentCmd.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Payment\Note;
+
+use App\Entity\TransactionOriginEnum;
+use DateTimeImmutable;
+
+class PaymentCmd extends BankCmd
+{
+    use CmdForgeTrait;
+
+    protected readonly int $transactionId;
+    protected readonly DateTimeImmutable $createdAt;
+    private readonly string $callBackToken;
+
+    public function __construct(
+        protected readonly int $invoiceId,
+        protected readonly int $userId,
+        protected readonly string $requestedRole,
+        protected readonly TransactionOriginEnum $origin,
+        protected readonly int $amount,
+        protected readonly string $note,
+    )
+    {
+    }
+
+    public function getRequestedRole(): string
+    {
+        return $this->requestedRole;
+    }
+
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function getNote(): string
+    {
+        return $this->note;
+    }
+
+    public function getCallbackToken(): string
+    {
+        if(isset($this->callBackToken))
+        {
+            return $this->callBackToken;
+        }
+
+        if(!isset($this->createdAt) or !isset($this->transactionId))
+        {
+            throw new \Exception('Cannot create callback token before persisting request');
+        }
+
+        $public = strval($this->getCreatedAt()->getTimestamp()) . strval($this->getAmount());
+        $private = substr(md5('PaymentRequest$' . $public . '$'), 10);
+        $token = base64_encode($private . '.' . $public);
+
+        $this->callBackToken = $token;
+
+        return $this->callBackToken;
+    }
+}
