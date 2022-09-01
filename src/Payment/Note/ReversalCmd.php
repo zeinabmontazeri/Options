f717d6e2067d56bb072938f3db1fc6bf80684798
S. Sadegh Marashi <s.s.marashi@gmail.com>
2022-09-01 14:40:37 +0430

    feat#10: Add Reversal command

---
+++ src/Payment/Note/ReversalCmd.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Payment\Note;
+
+class ReversalCmd extends BankCmd
+{
+    use CmdForgeTrait;
+
+    protected readonly int $transactionId;
+    protected readonly \DateTimeImmutable $createdAt;
+
+    public function __construct(
+        private readonly int $paymentTransactionId,
+        private readonly string $bankToken,
+    ) {
+    }
+
+    public function getPaymentTransactionId()
+    {
+        return $this->paymentTransactionId;
+    }
+
+    public function getBankToken()
+    {
+        return $this->bankToken;
+    }
+}
