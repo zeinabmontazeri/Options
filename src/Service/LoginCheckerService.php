f7d3edcdbe649fa8791b75beb179c57e057ed7d8
S. Sadegh Marashi <s.s.marashi@gmail.com>
2022-09-04 22:57:39 +0430

    feat#3: Refactore Login checker

--- src/Service/LoginCheckerService.php
+++ src/Service/LoginCheckerService.php
@@ -6,31 +6,17 @@ use App\Entity\User;
 
 class LoginCheckerService
 {
-    public function check(string $loginRole, string|array $userRole): bool
+    public function check(string $loginRole, array $userRole): bool
     {
         if (!array_key_exists($loginRole, User::ROLE_HIERARCHY))
             return false;
 
-        if (is_array($userRole)) {
-            if (count($userRole) == 0) {
-                $userRole[] = User::ROLE_EXPERIENCER;
-            }
-
-            $userRole = array_map('strtoupper', $userRole);
-
-            // expand roles
-            $expanded_roles = [];
-            foreach ($userRole as $role) {
-                $expanded_roles = array_merge($expanded_roles, User::ROLE_HIERARCHY[$role]);
-            }
-            $userRole = array_unique($expanded_roles);
-        } elseif (is_string($userRole)) {
-            $userRole = User::ROLE_HIERARCHY[strtoupper($userRole)];
+        if (count($userRole) === 0) {
+            $userRole[] = User::ROLE_EXPERIENCER;
         }
 
-        if (!in_array($loginRole, $userRole))
-            return false;
+        $userRoles = User::ROLE_HIERARCHY[strtoupper($userRole[0])];
 
-        return true;
+        return in_array($loginRole, $userRoles);
     }
 }
