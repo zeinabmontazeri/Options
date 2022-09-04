138428ef1a975434f3d0dd237214adb2b4e409d4
aghaSeyed <mahdisetayande@yahoo.com>
2022-09-04 14:18:13 +0430

    feat(back) : #22 add unitTest for Host/ExperienceConroller

--- tests/Controller/AdminCategoryControllerTest.php
+++ tests/Controller/AdminCategoryControllerTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 namespace App\Tests\Controller;
 
 use App\Entity\User;
-use Exception;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 
 class AdminCategoryControllerTest extends BaseTestCase
