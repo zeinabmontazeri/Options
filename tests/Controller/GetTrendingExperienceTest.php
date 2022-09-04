7160f76ac3fefa8a0c67cfb2a2d93b162451f643
ElmiraBehnava <elmira.behnava@yahoo.com>
2022-09-04 22:39:20 +0430

    feat(back): trending test

---
+++ tests/Controller/GetTrendingExperienceTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Entity\Experience;
+
+class GetTrendingExperienceTest extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setFixtureFromSourceName(['GetTrendingExperienceFixture']);
+    }
+
+    public function testGetTrendingExperience()
+    {
+        $experiences = $this->entityManager->getRepository(Experience::class)->getTrendingExperiences();
+
+        $this->client->request('GET', '/api/v1/shop/experiences/trending/', []
+            , [], [
+                'CONTENT_TYPE' => 'application/json',
+            ]);
+        $response = $this->client->getResponse();
+        $decodedResponse = json_decode($response->getContent());
+        $this->assertResponseCode($response, 200);
+        $this->assertSameSize($experiences, $decodedResponse->data);
+    }
+}
