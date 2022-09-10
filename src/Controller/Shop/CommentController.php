b5b6eb919047a805b21ff727a299eded11bf6457
ElmiraBehnava <elmira.behnava@yahoo.com>
2022-09-11 00:19:37 +0430

    feat(back): fix uris

--- src/Controller/Shop/CommentController.php
+++ src/Controller/Shop/CommentController.php
@@ -14,7 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('api/v1/shop')]
+#[Route('api/v1')]
 class CommentController extends AbstractController
 {
     /**
