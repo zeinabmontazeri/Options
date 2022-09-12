274c3dc01258ffebbc9c01c9e65133f47a3118fb
mehdad <m.mohamadi313@gmail.com>
2022-09-12 09:53:00 +0430

    Add basics for host approval

--- src/Controller/Admin/HostController.php
+++ src/Controller/Admin/HostController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Auth\AcceptableRoles;
+use App\Entity\Enums\EnumPermissionStatus;
 use App\Entity\Host;
+use App\Entity\UpgradeRequest;
 use App\Entity\User;
 use App\Repository\HostRepository;
+use App\Repository\UpgradeRequestRepository;
 use App\Request\AuthorizeAdminRequest;
+use App\Request\HostApproveRequest;
 use App\Request\HostAuthorizationFilterRequest;
 use App\Service\Admin\HostApprovalService;
 use App\Service\Admin\GetHostAuthorizationStatusByFilterService;
@@ -14,6 +18,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('api/v1/admin')]
@@ -25,6 +30,26 @@ class HostController extends AbstractController
         HostApprovalService $service): JsonResponse
     {
 
+        $test = $service->getPendingList($page=1,$perPage = 20);
+        return $this->json([
+            'data' => $test,
+            'message' => 'successfully got pending requests',
+            'status' => 'success',
+        ], Response::HTTP_OK);
+    }
+
+    #[Route('/hosts/approve', name: 'app_admin_host_approve_host',methods: 'POST')]
+//    #[AcceptableRoles(User::ROLE_ADMIN)]
+    public function approveHost(
+        HostApproveRequest $approveRequest,
+        UpgradeRequestRepository $repository,
+        HostApprovalService $service): JsonResponse
+    {
+        $upgradeRequest = $repository->find($approveRequest->request_id);
+        if(!$upgradeRequest) throw new BadRequestHttpException('The Requested resource not found');
+        if($upgradeRequest->getStatus() != EnumPermissionStatus::PENDING) throw new BadRequestHttpException('The request already applied');
+
+dd($upgradeRequest);
         $test = $service->getPendingList($page=1,$perPage = 20);
         return $this->json([
             'data' => $test,
